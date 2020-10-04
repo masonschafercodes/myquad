36449fbae8b3824eef8f0458809da6f4c124fa82
Mason Schafer <mason@Masons-MacBook-Pro.local>
2020-10-04 11:13:50 -0400

    stylez baby

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import MailchimpForm from './components/MailchimpForm/MailchimpForm';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
-      <img src="https://i.ibb.co/TDf0NTJ/logo-option-2exports.png" alt="logo-option-2" />
-      <p>The App that Reimagines Social Life on Campus.</p>
-      <h1>Subscribe below for Updates!</h1>
+      <img src="https://i.ibb.co/TDf0NTJ/logo-option-2exports.png" id="landing-img" alt="logo-option-2" />
+      <p id="describer">The App that Reimagines Social Life on Campus.</p>
+      <h1 id="capture">Subscribe below for Updates!</h1>
       <MailchimpForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="Footer">
+        <p>Made by MyQuad &#169; 2020</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
